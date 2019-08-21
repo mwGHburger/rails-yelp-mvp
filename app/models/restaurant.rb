536710f5668ed2607391a9c2a6a3e29b9fa290fe
Michael Wang <michaelw0395@gmail.com>
2019-08-21 16:42:37 +1000

    It is functional

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+  # When a restaurant is destroyed, all of its reviews should be destroyed as well.
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating 5 restaurants"
+restaurant_attributes = [
+  {
+    name: "Restaurant1",
+    address: "123 fake street",
+    phone_number: "12345",
+    category: "chinese"
+  },
+  {
+    name: "Restaurant2",
+    address: "456 cake street",
+    phone_number: "2342",
+    category: "italian"
+  },
+  {
+    name: "Restaurant3",
+    address: "789 pie street",
+    phone_number: "345432",
+    category: "japanese"
+  },
+  {
+    name: "Restaurant4",
+    address: "111 yum street",
+    phone_number: "54342",
+    category: "belgian"
+  },
+  {
+    name: "Restaurant5",
+    address: "312 yay street",
+    phone_number: "4654",
+    category: "french"
+  }
+]
+Restaurant.create!(restaurant_attributes)
+puts "Finished!!!"
